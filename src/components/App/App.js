e85d9884936c68ff1a6f7f9917e0cdf86c2e531f
cficht <chris.ficht@gmail.com>
2020-08-07 11:59:46 -0700

    created function in guardian api to get articles and a test. also set up a .env and eslint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { getArticles } from '../../services/guardian-api'
 import './App.css';
 
 function App() {
+  const articles = () => {
+    getArticles()
+    .then(res => console.log(res))
+  }
+
+  articles();
+
   return (
     <div className="App">
       Guardian Reader
